502134d5dcf8b34c0994721096972c6c6d519482
Najim Rizky <najim.rizky@student.umn.ac.id>
2020-12-29 20:03:12 +0700

    Layout Calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+//import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import React from 'react';
 
 function App() {
   return (
@@ -6,7 +9,108 @@ function App() {
       <header className="App-header">
         <h1>Calculator</h1>
         <div className="baseContainer">
-          <div className="contain">
+          <Grid container item xs={12} spacing={0}>
+            <Grid item xs={12}>
+              <div className="results">0</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnSym">%</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">C</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">CE</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">Del</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnSym">1/x</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">x²</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">√x</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">÷</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnNum">7</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">8</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">9</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">×</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnNum">4</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">5</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">6</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">-</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnNum">1</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">2</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">3</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">+</div>
+            </Grid>
+
+            <Grid item xs={3}>
+              <div className="btnSym">+/-</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnNum">0</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnSym">.</div>
+            </Grid>
+            <Grid item xs={3}>
+              <div className="btnEq">=</div>
+            </Grid>
+          </Grid>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+      /*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>*/
+
+      /*<div className="contain">
             0
           </div>
           <div className="contain">
@@ -44,21 +148,5 @@ function App() {
             <li>0</li>
             <li>.</li>
             <li>=</li>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-      /*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>*/
+          </div> 
+      */
